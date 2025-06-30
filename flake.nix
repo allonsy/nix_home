c48fc8dda5289b4833b28d8abbcc7049eca83935
Alec Snyder <linuxbash8@gmail.com>
2025-06-30 22:33:50 +0300

    fix more indents

--- flake.nix
+++ flake.nix
@@ -11,28 +11,28 @@
     let
       systems = import ./systems.nix;
     in
-        systems.forEachSystem (system:
+      systems.forEachSystem (system:
         let
-            pkgs = import nixpkgs { inherit system; };
-            dotfiles = (import ./dotfiles/dotfiles.nix).package pkgs system;
+          pkgs = import nixpkgs { inherit system; };
+          dotfiles = (import ./dotfiles/dotfiles.nix).package pkgs system;
         in {
-            packages.default = pkgs.buildEnv {
-                name = "home";
-                version = "2";
-                paths = with pkgs; [
-                    eza
-                    jujutsu
-                    nix
-                    starship
-                    uv
-                    zsh
-                    kitty
-                    nixGL.packages.${system}.nixGLIntel
+          packages.default = pkgs.buildEnv {
+            name = "home";
+            version = "2";
+            paths = with pkgs; [
+              eza
+              jujutsu
+              nix
+              starship
+              uv
+              zsh
+              kitty
+              nixGL.packages.${system}.nixGLIntel
 
-                    # custom packages
-                    dotfiles
-                ];
-            };
+              # custom packages
+              dotfiles
+            ];
+          };
         }
-        );
+      );
 }
