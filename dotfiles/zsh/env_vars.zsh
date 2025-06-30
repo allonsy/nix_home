2f885b0326c9e7a36e4259f71df2453bc1c18970
Alec Snyder <linuxbash8@gmail.com>
2025-06-30 23:35:32 +0300

    remove nvm from env vars

--- dotfiles/zsh/env_vars.zsh
+++ dotfiles/zsh/env_vars.zsh
@@ -1,7 +1,3 @@
 export LC_ALL=en_US.UTF-8
-export NVM_DIR=~/.nvm
-export NVM_CD_FLAGS=-q
-export NVM_BIN=~/.nvm/versions/node/v14.18.1/bin
-export NVM_INC=~/.nvm/versions/node/v14.18.1/include/node
 export CARGO_INSTALL_ROOT=~/.local
-export PATH=~/.nix-profile/bin:~/.nvm/versions/node/v14.18.1/bin:~/.local/bin:$PATH
+export PATH=~/.nix-profile/bin:~/.local/bin:$PATH
