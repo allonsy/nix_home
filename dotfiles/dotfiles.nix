e4675fc38edf15990cf8c269aee6068f9e05107f
Alec Snyder <linuxbash8@gmail.com>
2025-06-30 22:28:57 +0300

    fix indentation in dotfiles

--- dotfiles/dotfiles.nix
+++ dotfiles/dotfiles.nix
@@ -1,28 +1,28 @@
 {
   package = nixpkgs: system:
     nixpkgs.stdenv.mkDerivation {
-    pname = "home_dotfiles";
-    version = "2";
-    src = ./.;
+      pname = "home_dotfiles";
+      version = "2";
+      src = ./.;
 
-    installPhase = ''
-    # zsh
-    mkdir -p $out/usr/config/zsh
-    mkdir -p $out/usr/config/starship
-    cp zsh/* $out/usr/config/zsh
-    cp zsh/starship.toml $out/usr/config/starship/config.toml
+      installPhase = ''
+        # zsh
+        mkdir -p $out/usr/config/zsh
+        mkdir -p $out/usr/config/starship
+        cp zsh/* $out/usr/config/zsh
+        cp zsh/starship.toml $out/usr/config/starship/config.toml
 
-    # jujutsu
-    mkdir -p $out/usr/config/jj
-    cp jujutsu/config.toml $out/usr/config/jj/config.toml
+        # jujutsu
+        mkdir -p $out/usr/config/jj
+        cp jujutsu/config.toml $out/usr/config/jj/config.toml
 
-    # nix
-    mkdir -p $out/usr/config/nix
-    cp nix/nix.conf $out/usr/config/nix
+        # nix
+        mkdir -p $out/usr/config/nix
+        cp nix/nix.conf $out/usr/config/nix
 
-    # kitty
-    mkdir -p $out/usr/config/kitty
-    cp kitty/* $out/usr/config/kitty
-    '';
-  };
+        # kitty
+        mkdir -p $out/usr/config/kitty
+        cp kitty/* $out/usr/config/kitty
+      '';
+    };
 }
