9c555cf9a12a9a990985405201ca8dfe84f31182
Alec Snyder <alec.snyder@at-bay.com>
2025-07-07 09:47:09 +0300

    set ld library path vars for homebrew

--- dotfiles/zsh/env_vars.zsh
+++ dotfiles/zsh/env_vars.zsh
@@ -9,3 +9,5 @@ export HOMEBREW_CELLAR=/opt/homebrew/Cellar
 export HOMEBREW_REPOSITORY=/opt/homebrew
 export INFOPATH=/opt/homebrew/share/info:$INFOPATH
 export KUBECONFIG=~/.kube/config-atbay
+export LD_LIBRARY_PATH=/opt/homebrew/lib:$LD_LIBRARY_PATH
+export DYLD_LIBRARY_PATH=/opt/homebrew/lib:$DYLD_LIBRARY_PATH
