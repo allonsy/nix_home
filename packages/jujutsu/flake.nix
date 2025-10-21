819ff5fc3a1387dc6e98166e1828bca9efcc7f09
Alec Snyder <alec.snyder@at-bay.com>
2025-10-21 12:47:20 +0300

    Add custom zsh completions

--- packages/jujutsu/flake.nix
+++ packages/jujutsu/flake.nix
@@ -19,6 +19,7 @@
           installPhase = ''
             mkdir -p $out/usr/config/jj
             mkdir -p $out/bin
+            mkdir -p $out/share/zsh/site-functions
 
             cat <<EOF > $out/usr/config/jj/config.toml
             [user]
@@ -29,6 +30,7 @@
 
             cat config.toml >> $out/usr/config/jj/config.toml
             cp ${pkgs.jujutsu}/bin/jj $out/bin/jj
+            cp ${pkgs.jujutsu}/share/zsh/site-functions/_jj $out/share/zsh/site-functions/_jj
           '';
         };
     };
